645ca062e96cf801ec70043970749bd3a32a231a
MagalieHeinrich <maheinri@students.uni-mainz.de>
2026-04-29 23:41:02 +0200

    Added DIRK methods from Hairer/Wanner book
    Added DIRK methods from E. Hairer, G. Wanner. Solving ordinary differential equations II: Stiff and Differential-Algebraic Problems

--- libs/Theseus/src/dirk/tableau.jl
+++ libs/Theseus/src/dirk/tableau.jl
@@ -99,3 +99,161 @@ function RKTableau(alg::DIRK43, RealT)
     c[3] = 1 // 2 - 1 / (2 * sqrt(convert(RealT, 2)))
     return DIRKButcher(a, b, c)
 end
+
+"""
+CooperSayfy5
+A 5th-order, 5-stage, A-stable DIRK method.
+
+## References
+-E. Hairer, G. Wanner. Solving ordinary differential equations II: Stiff and Differential-Algebraic Problems.
+Springer, 1996. page.101
+"""
+
+struct CooperSayfy5 <: DIRK{5} end
+
+function RKTableau(alg::CooperSayfy5, RealT)
+    nstage = 5
+    sqrt6 = sqrt(convert(RealT, 6))
+    
+    γ = (6 - sqrt6) / 10
+    
+    a = zeros(RealT, nstage, nstage)
+    
+    a[1, 1] = γ
+
+    a[2, 1] = (6 + 5 * sqrt6) / 14
+    a[2, 2] = γ
+    
+    a[3, 1] = (888 + 607 * sqrt6) / 2850
+    a[3, 2] = (126 - 161 * sqrt6) / 1425
+    a[3, 3] = γ
+
+    a[4, 1] = (3153 - 3082 * sqrt6) / 14250
+    a[4, 2] = (3213 + 1148 * sqrt6) / 28500
+    a[4, 3] = (-267 + 88 * sqrt6) / 500
+    a[4, 4] = γ
+
+    a[5, 1] = (-32583 + 14638 * sqrt6) / 71250
+    a[5, 2] = (-17199 + 364 * sqrt6) / 142500
+    a[5, 3] = (1329 - 544 * sqrt6) / 2500
+    a[5, 4] = (-96 + 131 * sqrt6) / 625
+    a[5, 5] = γ
+    
+    b = zeros(RealT, nstage)
+    b[1] = 0
+    b[2] = 0
+    b[3] = 1 // 9
+    b[4] = (16 - sqrt6) / 36
+    b[5] = (16 + sqrt6) / 36
+    
+    c = zeros(RealT, nstage)
+    c[1] = γ
+    c[2] = (6 + 9 * sqrt6) / 35  # Corrected from the row 2 c-column
+    c[3] = 1 
+    c[4] = (4 - sqrt6) / 10
+    c[5] = (4 + sqrt6) / 10
+    
+    return DIRKButcher(a, b, c)
+end
+
+
+"""
+HairerWannerSDIRK4
+A 4th-order, 5-stage, L-stable SDIRK method.
+
+## References
+-E. Hairer, G. Wanner. Solving ordinary differential equations II: Stiff and Differential-Algebraic Problems.
+Springer, 1996. page.100
+"""
+
+struct HairerWannerSDIRK4 <: DIRK{5} end
+
+function RKTableau(alg::HairerWannerSDIRK4, RealT)
+    nstage = 5
+    a = zeros(RealT, nstage, nstage)
+    
+    γ = 1 // 4
+    
+    a[1, 1] = γ
+    
+    a[2, 1] = 1 // 2
+    a[2, 2] = γ
+    
+    a[3, 1] = 17 // 50
+    a[3, 2] = -1 // 25
+    a[3, 3] = γ
+    
+    a[4, 1] = 371 // 1360
+    a[4, 2] = -137 // 2720
+    a[4, 3] = 15 // 544
+    a[4, 4] = γ
+    
+    a[5, 1] = 25 // 24
+    a[5, 2] = -49 // 48
+    a[5, 3] = 125 // 16
+    a[5, 4] = -85 // 12
+    a[5, 5] = γ
+
+    b = zeros(RealT, nstage)
+    b[1] = 25 // 24
+    b[2] = -49 // 48
+    b[3] = 125 // 16
+    b[4] = -85 // 12
+    b[5] = 1 // 4
+    
+    c = zeros(RealT, nstage)
+    c[1] = 1 // 4
+    c[2] = 3 // 4
+    c[3] = 11 // 20
+    c[4] = 1 // 2
+    c[5] = 1
+    
+    return DIRKButcher(a, b, c)
+end
+
+
+"""
+CrouzeixRaviart34
+A 4th-order, 3-stage, L-stable SDIRK method.
+
+## References
+-E. Hairer, G. Wanner. Solving ordinary differential equations II: Stiff and Differential-Algebraic Problems.
+Springer, 1996. page.100
+"""
+
+struct CrouzeixRaviart34 <: DIRK{3} end
+
+function RKTableau(alg::CrouzeixRaviart34, RealT)
+    nstage = 3
+    
+    sqrt3 = sqrt(convert(RealT, 3))
+    π_val = convert(RealT, π)
+    γ = (1 / sqrt3) * cos(π_val / 18) + 1 // 2
+
+    δ = 1 / (6 * (2 * γ - 1)^2)
+    
+    a = zeros(RealT, nstage, nstage)
+
+    a[1, 1] = γ
+
+    a[2, 1] = 1 // 2 - γ
+    a[2, 2] = γ
+
+    a[3, 1] = 2 * γ
+    a[3, 2] = 1 - 4 * γ
+    a[3, 3] = γ
+    
+    b = zeros(RealT, nstage)
+    b[1] = δ
+    b[2] = 1 - 2 * δ
+    b[3] = δ
+    
+    c = zeros(RealT, nstage)
+    c[1] = γ
+    c[2] = 1 // 2
+    c[3] = 1 - γ
+    
+    return DIRKButcher(a, b, c)
+end
+    
+
